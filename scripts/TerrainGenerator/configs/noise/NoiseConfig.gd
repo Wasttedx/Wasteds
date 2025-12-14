b2a92ed2d78e3dfdb06eb73082d24a36adc79cde
Wasted <wastted@proton.me>
2025-12-14 16:15:42 +0330

    Better Configs
    +New Textures

--- scripts/TerrainGenerator/configs/noise/NoiseConfig.gd
+++ scripts/TerrainGenerator/configs/noise/NoiseConfig.gd
@@ -2,10 +2,10 @@ extends Resource
 class_name NoiseConfig
 
 @export_group("Noise Parameters")
-@export var noise_seed: int = 1337
-@export var frequency: float = 0.01
-@export var octaves: int = 4
-@export var lacunarity: float = 2.0
-@export var persistence: float = 0.5
-@export var height_multiplier: float = 20.0
-@export var noise_offset: Vector2 = Vector2.ZERO
+@export var noise_seed: int = 84291
+@export var frequency: float = 0.004
+@export var octaves: int = 5
+@export var lacunarity: float = 2.1
+@export var persistence: float = 0.45
+@export var height_multiplier: float = 35.0
+@export var noise_offset: Vector2 = Vector2(1000, 1000)
